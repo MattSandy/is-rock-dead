78604eb07a56912a6a4dd3072c72cc8c489ea1b3
Taras Kaduk <taras.kaduk@gmail.com>
2019-10-23 14:46:07 -0400

    init

---
+++ analysis.R
@@ -0,0 +1,141 @@
+
+theme_set(theme_fivethirtyeight()+
+            theme(rect = element_blank(),
+                  panel.border = element_blank(),
+                  legend.title = element_blank(),
+                  strip.background = element_blank()))
+
+
+albums <- read_csv("albums.csv")
+tags <- read_csv("tags.csv")
+lastfm_tags <- read_csv("lastfm_tags.csv")
+tags_reclass <- read_csv("tags_reclass.csv")
+
+df <- albums %>% 
+  left_join(lastfm_tags, by = "search") %>% 
+  left_join(tags_reclass, by = c("name" = "tag")) %>% 
+  rename(tag = name) %>% 
+  mutate(year = year(date)) %>% 
+  filter(!(artist == "Domingo" & tag == "hip-hop"))
+
+top_tags <- df %>% 
+  select(tag , category) %>% 
+  group_by(tag, category) %>% 
+  tally()
+
+
+
+# Charts ------------------------------------------------------------------
+
+# df %>% 
+#   filter(category %in% c("rock", "hip-hop")) %>% 
+#   group_by(category, year) %>% 
+#   tally() %>% 
+#   ungroup() %>% 
+#   ggplot(aes(x = year, y = n, col = category)) + 
+#   geom_line() 
+
+
+
+
+
+
+#path <- "/Users/taras/R/taraskaduk/content/posts/2019-10-rock-is-dead/"
+path <- ""
+
+draw_chart <- function(data, yaxis, title) {
+  ggplot(data, aes_string(x = "year", y = yaxis, col = "tag")) + 
+    geom_line(size = 1) +
+    scale_color_brewer(palette = "Set1", type = "qual") + 
+    scale_x_continuous(breaks = c(1963,  1980, 2000, 2018)) +
+    labs(
+      title = title,
+      subtitle = "Years 1963 - 2018",
+      caption = "Sources: Billboard 200 charts, Last FM
+    See full methodology and code at taraskaduk.com
+    
+    taraskaduk.com | @taraskaduk"
+    )
+}
+
+df %>% 
+  filter(tag %in% c("rock", "hip-hop")) %>% 
+  group_by(tag, year) %>% 
+  tally() %>% 
+  ungroup() %>% 
+  draw_chart(yaxis = "n", title = "Count of albums appearing in weekly Billboard 200 charts")
+ggsave(paste0(path,"1.png"))
+
+
+
+df %>% 
+  filter(tag %in% c("rock", "hip-hop")) %>% 
+  select(tag, year, artist) %>% 
+  distinct() %>% 
+  group_by(tag, year) %>% 
+  tally() %>% 
+  ungroup() %>% 
+  draw_chart(yaxis = "n", title = "Count of distict artists appearing in Billboard 200 every year")
+ggsave(paste0(path,"2.png"))
+
+df %>% 
+  filter(tag %in% c("rock", "hip-hop")) %>% 
+  select(tag, year, artist, age, type) %>% 
+  distinct() %>% 
+  group_by(tag, year, type) %>% 
+  summarise(age = median(age)) %>% 
+  ungroup() %>% 
+  draw_chart(yaxis = "age", title = "Median age of artists and groups in Billboard 200") +
+  facet_wrap(~type, scales = "free")
+ggsave(paste0(path,"3.png"))
+
+df %>% 
+  filter(tag %in% c("rock", "hip-hop")) %>% 
+  group_by(tag, year) %>% 
+  tally() %>% 
+  ungroup() %>% 
+ggplot(aes(x = year, y = n, col = tag)) + 
+  geom_line(size = 1) +
+  scale_color_brewer(palette = "Set1", type = "qual") + 
+  #labs(title = "Is Rock Dying?") +
+  #scale_x_continuous(breaks = c(1963,  1980, 2000, 2018)) +
+  theme(rect = element_blank(),
+        panel.border = element_blank(),
+        strip.background = element_blank(),
+        axis.text = element_blank(),
+        axis.title=element_blank(),
+        panel.grid.major = element_blank(),
+        # legend.position="top",
+        # legend.key.size = unit(1, "cm"),
+        # legend.text=element_text(size=14),
+        legend.position = "none")
+ggsave(paste0(path,"cover.png"), width = 100, height = 75, units = "mm")
+
+# df %>% 
+#   filter(tag %in% c("rock", "hip-hop")) %>% 
+#   select(tag, year, artist,type, age) %>% 
+#   group_by(artist) %>% 
+#   mutate(filt = min(year) == year) %>%
+#   ungroup() %>% 
+#   filter(filt == 1) %>% 
+#   distinct() %>% 
+#   group_by(tag, year, type) %>% 
+#   summarise(age = mean(age)) %>% 
+#   ungroup() %>% 
+#   draw_chart(yaxis = "age", title = "Mean age upon first entrance") +
+#   facet_wrap(~type, scales = "free")
+
+
+# df %>% 
+#   filter(tag %in% c("rock", "hip-hop")) %>% 
+#   filter(rank == 1) %>% 
+#   select(tag, year, artist) %>% 
+#   distinct() %>% 
+#   group_by(tag, year) %>% 
+#   tally() %>% 
+#   ungroup() %>% 
+#   
+#   ggplot(aes(x = year, y = n, col = tag)) + 
+#   geom_line() 
+
+      
---
+++ get-data.R
@@ -0,0 +1,133 @@
+# https://components.one/datasets/billboard-200/
+# https://www.dropbox.com/s/ahog97hcatpiddk/billboard-200.db?dl=1
+
+library(RSQLite)
+library(tidyverse)
+library(jsonlite)
+library(lubridate)
+library(ggthemes)
+
+
+
+# Get Billboard 200 data --------------------------------------------------
+
+
+curl::curl_download("https://www.dropbox.com/s/ahog97hcatpiddk/billboard-200.db?dl=1",
+                    destfile = "bb200.db")
+sqlite.driver <- dbDriver("SQLite")
+db <- dbConnect(sqlite.driver,
+                dbname = "bb200.db")
+albums <- dbReadTable(db,"albums") %>%
+  filter(!is.na(artist) &
+           artist != "" &
+           !(artist %in% c("Soundtrack", "Various Artists", "Original Cast")) &
+           !is.na(date)) %>%
+  mutate(search = map_chr(.$artist, URLencode, reserved = TRUE))
+#acoustic_features <- dbReadTable(db,"acoustic_features")
+dbDisconnect(db)
+
+artists <- albums %>%
+  group_by(artist, search) %>%
+  tally() %>%
+  ungroup() %>% 
+  arrange(desc(n))
+
+
+# get_artist_mbid <- function(search) {
+#   url <- paste0("https://musicbrainz.org/ws/2/artist/?query=artist:", search, "&limit=1&fmt=json")
+#   json <- fromJSON(url, flatten = TRUE) %>% as_tibble() %>% flatten()
+#   json$search <- search
+#   return(json)
+# }
+# artists_result <- map_dfr(artists$search, possibly(get_artist_mbid, otherwise = NULL))
+# missed_artists <- artists %>% 
+#   anti_join(artists_result, by = "search")
+# more_results <- map_dfr(missed_artists$search, possibly(get_artist_mbid, otherwise = NULL))
+# artists_result <- bind_rows(artists_result, 
+#                             more_results)
+# 
+# saveRDS(artists_result, "artist_result.RDS")
+artists_result <- readRDS("artist_result.RDS")
+
+artists_cleaned <- artists_result %>% 
+  select(id = artists.id,
+         search, 
+         type = artists.type,
+         name = artists.name,
+         tags = artists.tags,
+         start_year = `artists.life-span.begin`) %>% 
+  mutate(start_year = as.integer(substr(start_year, 1, 4)))
+
+albums_cleaned <- albums %>% 
+  rename(album_index = id) %>% 
+  left_join(artists_cleaned %>% select(id, search, start_year, type), by = "search") %>%
+  mutate(date = as.Date(date),
+         chart_year = year(date),
+         age = chart_year - start_year) %>% 
+  ## Bad data
+  filter(date < "2019-01-01" & ((age >= 0 & type == "Group") | (age >= 10 & type == "Person")))
+
+
+write_csv(albums_cleaned, "albums.csv")
+
+
+
+# Last FM tags ------------------------------------------------------------
+
+## Insert your own user name
+api_user <- "diezzz_htc"
+## Insert your API key
+api_key <- Sys.getenv("LASTFM_API")
+
+
+get_tags <- function(search) {
+  api_artist_tags <- paste0(
+    "https://ws.audioscrobbler.com/2.0/",
+    "?method=artist.gettoptags", 
+    "&artist=", search,
+    #"&mbid=", artist_mbid ,
+    "&api_key=", api_key,
+    "&format=json"
+  )
+  
+  error <- fromJSON(api_artist_tags)$error
+  tags <- fromJSON(api_artist_tags)$toptags$tag
+  
+  if (!is.null(error) || length(tags) == 0) {
+    return(NULL)
+  } else {
+    api_response <- fromJSON(api_artist_tags) %>%
+      .$toptags %>%
+      .$tag %>%
+      filter(name != "seen live") %>%
+      head(5) %>%
+      mutate(search = search,
+             name = tolower(name)) %>%
+      select(-url)
+    return(api_response)
+  }
+}
+
+
+lastfm_tags <- read_csv("lastfm_tags.csv")
+
+tags_temp <- artists %>% 
+  select(search) %>% 
+  anti_join(lastfm_tags, by = "search") %>% 
+  as_vector() %>% 
+  map_dfr(get_tags)
+
+lastfm_tags <- bind_rows(lastfm_tags, tags_temp)
+remove(tags_temp)
+write_csv(lastfm_tags, "lastfm_tags.csv")
+
+top_tags <- lastfm_tags %>% 
+  group_by(name) %>% 
+  tally %>% 
+  ungroup() %>% 
+  filter(n >= 25)
+  
+lastfm_tags %>% 
+  semi_join(top_tags, by = "name") %>% 
+  write_csv("tags.csv")
+
